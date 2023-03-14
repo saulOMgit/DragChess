a9b4799786de46e5fd8449cdbbaff351751cd077
saulOMgit <sauloteromelchor@gmail.com>
2023-03-14 19:40:32 +0100

    Casillas ampliadas

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,76 @@
 window.onload= function(){
-    const rey=document.querySelector(".pieza");
+    const piezas=document.querySelectorAll(".pieza");
     const casillas= document.querySelectorAll(".square");
 
-    casillas.forEach(casilla => {
+    // casillas.forEach(casilla => {
+    //     casilla.addEventListener("dragover",dragOver);
+    //     casilla.addEventListener("drop",dragDrop);
+    //     // 
+    //     casilla.addEventListener("dragenter",dragEnter);
+    //     casilla.addEventListener("dragleave", dragLeave);
+    //     casilla.addEventListener("dragend", dragEnd);
+    // });
+
+    for(let pieza of piezas){
+         pieza.addEventListener("dragstart",dragStart);
+         pieza.addEventListener("drag",dragging);
+    };
+
+    for(let casilla of casillas){
         casilla.addEventListener("dragover",dragOver);
         casilla.addEventListener("drop",dragDrop);
         // 
         casilla.addEventListener("dragenter",dragEnter);
         casilla.addEventListener("dragleave", dragLeave);
         casilla.addEventListener("dragend", dragEnd);
-    });
-    rey.addEventListener("dragstart",dragStart);
+    };
+    
+  
 
     let elementoarrastrado;
     // funcion de arrastrado basico
     function dragStart(e){
         elementoarrastrado=e.target;
+        e.dataTransfer.setData('text/plain', e.target.id);
+        console.log("c: "+e.target.id);
+        contador=0;
     }
 
     function dragOver(e){
         e.preventDefault();
+        
     }
     function dragDrop(e){
-        console.log(e.target);
-        e.target.append(elementoarrastrado);
+        console.log("t"+e.target);
+        const id = e.dataTransfer.getData('text/plain');
+       
+        const draggable=document.getElementById(id);
+    
+        if (contador==1 || id!="rey"){
+        e.target.append(draggable);    
+         }
         e.target.classList.remove("resaltado");
     }
 
     //Hasta aqui arriba lo mas basico
 
-    rey.addEventListener("drag",dragging);
+    
     const info =document.querySelector("#info");
-    function dragging(e){
-        info.textContent= "Estas arrastrando la pieza "+ elementoarrastrado.id;
+    function dragging(e){      
+        info.textContent= "Estas arrastrando la pieza "+ e.target.id;
+        
     }
 
     function dragEnter(e){
         e.target.classList.add("resaltado");
+        
     }
-
+    let contador=0;
     function dragLeave(e){
         e.target.classList.remove("resaltado");
+       
+        contador++;
+        console.log(contador);
     }
 
     function dragEnd(e){
